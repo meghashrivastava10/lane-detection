7b02deaf8119845589f58ab6dc000645211c485f
Megha Shrivastava <shrivastava.megha09@gmail.com>
2021-06-02 18:13:42 -0500

    bitwise operation

--- lane_detection.py
+++ lane_detection.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
+
 
 def region_of_interest(image):
     height = image.shape[0]
@@ -15,11 +17,12 @@ def region_of_interest(image):
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
-
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+    
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-
-cv2.imshow("result", region_of_interest(canny))
+cropped_image = region_of_interest(canny)
+cv2.imshow("result", cropped_image)
 cv2.waitKey(0)
